9800e9a64607fb461af7b78b7b64a04f4f69b087
Travis Thomas <travis.thomas@townsendsecurity.com>
2016-12-07 19:59:20 -0800

    Improve documentation and formatting of tests.

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from base64 import b64encode
 url = 'http://localhost:5000'
 post_url = url + '/post'
 posts_url = url + '/posts'
+header_json = {'content-type':'application/json'}
 
 class FlaskTestCase(unittest.TestCase):
 
@@ -14,9 +15,11 @@ class FlaskTestCase(unittest.TestCase):
         self.assertTrue(rsp.status_code == 404)
 
     def test_post_returns_200(self):
-        data = {"title":"test_post_returns_200", "body":"you posted!"}
-        rsp = requests.post(post_url, data=json.dumps(data), headers={
-            'content-type':'application/json'})
+        data = {
+            "title" : "test_post_returns_200", 
+            "body" : "you posted!"
+        }
+        rsp = requests.post(post_url, data=json.dumps(data), headers=header_json)
         self.assertTrue(rsp.status_code == 200)
 
     def test_posts_returns_200(self):
@@ -24,16 +27,22 @@ class FlaskTestCase(unittest.TestCase):
         self.assertTrue(rsp.status_code == 200)
 
     def test_post_shows_what_was_posted(self):
-        index = b64encode(os.urandom(4))
-        data = {"title":index, 
-            "body":"This has been posted and should be in the list of posts!"}
-        rsp = requests.post(post_url, data=json.dumps(data), headers={
-            'content-type':'application/json'})
+        '''
+        Create a randomized string to persist as a post's title. POST it, then
+        get the list of posts and find that string among the titles of the posts.
+        '''
+        index = b64encode(os.urandom(6))
+        data = {
+            "title" : index, 
+            "body" : "This has been posted and should be in the list of posts!"
+        }
+        rsp = requests.post(post_url, data=json.dumps(data), headers=header_json)
         self.assertTrue(rsp.status_code == 200)
+        
         rsp = requests.get(posts_url)
         found = False
         for post in rsp.json():
-            if index in post: found = True
+            if index == post['title']: found = True
         self.assertTrue(found)
 
         
